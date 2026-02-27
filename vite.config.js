a49669be7c967bd24d2c8730629fe05ef4083e58
neto <thomasconrod@gmail.com>
2026-02-26 19:10:06 -0800

    k

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
 	plugins: [sveltekit()],
 	ssr: {
-		noExternal: ['@strudel/web', '@strudel/core', '@strudel/mini', '@strudel/tonal', '@strudel/transpiler', '@strudel/webaudio']
+		external: ['@strudel/web']
 	}
 });
